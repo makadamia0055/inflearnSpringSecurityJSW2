7a9809d0623ec06e546af97802d83fb5b40a6331
makadamia0055 <makend91@naver.com>
2023-10-25 19:13:03 +0900

    project default setting

---
+++ coreSpringSecurity/src/main/java/com/css/corespringsecurity/controller/user/UserController.java
@@ -0,0 +1,12 @@
+package com.css.corespringsecurity.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping(value="/mypage")
+    public String myPage() throws Exception{
+        return "user/mypage";
+    }
+}
---
+++ coreSpringSecurity/src/test/java/com/css/corespringsecurity/CoreSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.css.corespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
