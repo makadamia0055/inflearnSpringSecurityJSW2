c248aa9a98255f8a4022f1c67a188d2e502c0fe4
makadamia0055 <makend91@naver.com>
2023-10-25 19:54:47 +0900

    complete lecture 3.1

--- coreSpringSecurity/src/main/java/com/css/corespringsecurity/controller/security/configs/SecurityConfig.java
+++ coreSpringSecurity/src/main/java/com/css/corespringsecurity/controller/security/configs/SecurityConfig.java
@@ -3,6 +3,10 @@ package com.css.corespringsecurity.controller.security.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -12,10 +16,39 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/mypage").hasRole("USER")
+                .antMatchers("/messages").hasRole("MANAGER")
+                .antMatchers("/config").hasRole("ADMIN")
                 .anyRequest().authenticated()
         .and()
                 .formLogin();
 
         return http.build();
+    }
+
+    //강의에서 쓰는 configure가 deprecated 되어서 이전 프로젝트에서 가지고 옴
+    @Bean
+    public static UserDetailsManager users() {
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}1111")
+                .roles("USER")
+                .build();
+
+        UserDetails sys = User.builder()
+                .username("manager")
+                .password("{noop}1111")
+                .roles("MANAGER", "USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}1111")
+                .roles("ADMIN", "MANAGER", "USER")
+                .build();
+
+        return new InMemoryUserDetailsManager( user, sys, admin );
     }
 }
